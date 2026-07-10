7ce73e290bfccaf3e3e04557731e862a006e59f1
Sebastian <sebastian090905@gmail.com>
2026-07-10 13:49:13 -0400

    fixed:name en fixture db

--- test/conftest.py
+++ test/conftest.py
@@ -47,7 +47,7 @@ def postgres_db_test():
 
     os.environ["DB_HOST"] = postgres.get_container_host_ip()
     os.environ["DB_PORT"] = str(postgres.get_exposed_port(5432))
-    os.environ["DB_USERNAME"] = postgres.username
+    os.environ["DB_USER"] = postgres.username
     os.environ["DB_PASSWORD"] = postgres.password
     os.environ["DB_NAME"] = postgres.dbname
 
